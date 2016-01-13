9f20c0f2922fe473e8b329a18f2ce54d9bd56e46
cladlink <michael.boutboul87@gmail.com>
2016-01-13 11:20:43 +0100

    V0

---
+++ src/ControlMouseOver.java
@@ -0,0 +1,28 @@
+/**
+ * Created by cladlink on 13/01/16.
+ */
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.*;
+
+public class ControlMouseOver extends Control implements MouseMotionListener
+{
+
+        public ControlMouseOver(Model model, Vue vue)
+        {
+            super(model, vue);
+            vue.setMouseControler(this);
+        }
+        public void mouseDragged(MouseEvent e)
+        {
+            System.out.println("coucou");
+        }
+        public void mouseMoved(MouseEvent e)
+        {
+            System.out.println("pu coucou");
+
+        }
+
+
+}
