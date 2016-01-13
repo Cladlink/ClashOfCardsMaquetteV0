231ed70f29b4393e2a9e79c4b44a9067c05100e1
cladlink <michael.boutboul87@gmail.com>
2016-01-13 09:58:12 +0100

    V0

--- src/Vue.java
+++ src/Vue.java
@@ -1,4 +1,4 @@
-// Affichage du plateau de jeu
+//Affichage du plateau de jeu
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,26 +7,26 @@ import javax.swing.border.*;
 
 public class Vue extends JFrame
 {
-	private int i, j ;
+    private int i, j ;
     // cartes du joueur pour la maquette
-	private ImageIcon[] teamBleue =
-	{
-		new ImageIcon(getClass().getResource("Images/Aiolia_Bleu.jpg")),
-		new ImageIcon(getClass().getResource("Images/Aioros_Bleu.jpg")),
-		new ImageIcon(getClass().getResource("Images/Aldebaran_Bleu.jpg")),
-		new ImageIcon(getClass().getResource("Images/Algol_Bleu.jpg")),
-		new ImageIcon(getClass().getResource("Images/Aphrodite_Bleu.jpg"))
-	};
+    private ImageIcon[] teamBleue =
+            {
+                    new ImageIcon(getClass().getResource("Images/Aiolia_Bleu.jpg")),
+                    new ImageIcon(getClass().getResource("Images/Aioros_Bleu.jpg")),
+                    new ImageIcon(getClass().getResource("Images/Aldebaran_Bleu.jpg")),
+                    new ImageIcon(getClass().getResource("Images/Algol_Bleu.jpg")),
+                    new ImageIcon(getClass().getResource("Images/Aphrodite_Bleu.jpg"))
+            };
 
     // carte de l'adversaire pour la maquette
-	private ImageIcon[] teamRouge =
-	{
-		new ImageIcon(getClass().getResource("Images/Aiolia_Rouge.jpg")),
-		new ImageIcon(getClass().getResource("Images/Aioros_Rouge.jpg")),
-		new ImageIcon(getClass().getResource("Images/Aldebaran_Rouge.jpg")),
-		new ImageIcon(getClass().getResource("Images/Algol_Rouge.jpg")),
-		new ImageIcon(getClass().getResource("Images/Aphrodite_Rouge.jpg"))
-	};
+    private ImageIcon[] teamRouge =
+            {
+                    new ImageIcon(getClass().getResource("Images/Aiolia_Rouge.jpg")),
+                    new ImageIcon(getClass().getResource("Images/Aioros_Rouge.jpg")),
+                    new ImageIcon(getClass().getResource("Images/Aldebaran_Rouge.jpg")),
+                    new ImageIcon(getClass().getResource("Images/Algol_Rouge.jpg")),
+                    new ImageIcon(getClass().getResource("Images/Aphrodite_Rouge.jpg"))
+            };
 
     // images des scores
     private ImageIcon scoreRouge;
@@ -34,47 +34,47 @@ public class Vue extends JFrame
 
 
     // éléments du chat
-	private JButton envoyer ;
-	private JTextField text ;
+    private JButton envoyer ;
+    private JTextField text ;
 
     // cartes des joueurs
-	private JLabel[] joueur;
-	private JLabel[] adversaire;
+    private JLabel[] joueur;
+    private JLabel[] adversaire;
 
     // scores des joueurs
     private JLabel scoreJoueur ;
     private JLabel scoreAdversaire ;
 
     // plateau de jeu
-	private JLabel[][] plateau;
+    private JLabel[][] plateau;
 
     // Les JPanels
-	private JPanel panoCartesJoueur;
-	private JPanel panoPlace;
-	private JPanel panoCartesAdversaire;
-	private JPanel panoPlateau;
-	private JPanel panoChat;
+    private JPanel panoCartesJoueur;
+    private JPanel panoPlace;
+    private JPanel panoCartesAdversaire;
+    private JPanel panoPlateau;
+    private JPanel panoChat;
     private JPanel panoJoueur;
     private JPanel panoAdversaire;
 
     // définition de la vue
-	public Vue(Model model)
-	{
-		initAttribut();
-		creerWidget();
+    public Vue(Model model)
+    {
+        initAttribut();
+        creerWidget();
 
-		setSize(1100,700);
-		setVisible(true);
-		setResizable(false);// important pour pas avoir a gérer la redifinition d'écran
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+        setSize(1100,700);
+        setVisible(true);
+        setResizable(false);// important pour pas avoir a gérer la redifinition d'écran
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-	public void initAttribut()
-	{
+    public void initAttribut()
+    {
 
-		envoyer = new JButton("Envoyer");
-		text = new JTextField();
-		text.setColumns(40);
+        envoyer = new JButton("Envoyer");
+        text = new JTextField();
+        text.setColumns(40);
 
         joueur = new JLabel[5];
         adversaire = new JLabel[5];
@@ -89,7 +89,7 @@ public class Vue extends JFrame
         scoreJoueur.setMaximumSize(new Dimension(100,100));
         scoreAdversaire.setMaximumSize(new Dimension(100,100));
 
-		plateau = new JLabel[3][3];
+        plateau = new JLabel[3][3];
 
         for(i=0; i<plateau.length;i++)
         {
@@ -99,18 +99,18 @@ public class Vue extends JFrame
                 plateau[i][j].setMaximumSize(new Dimension(50, 50));// 50 est un test
             }
         }
-		for(i=0; i<joueur.length; i++)
-		{
-			joueur[i] = new JLabel(teamBleue[i]);
-			adversaire[i] = new JLabel(teamRouge[i]);
-		}
-	}
+        for(i=0; i<joueur.length; i++)
+        {
+            joueur[i] = new JLabel(teamBleue[i]);
+            adversaire[i] = new JLabel(teamRouge[i]);
+        }
+    }
 
-	public void creerWidget()
-	{
+    public void creerWidget()
+    {
         // panel du plateau
-		panoPlateau = new JPanel(new GridLayout(3,3));
-		Border blackline = BorderFactory.createLineBorder(Color.black,1);
+        panoPlateau = new JPanel(new GridLayout(3,3));
+        Border blackline = BorderFactory.createLineBorder(Color.black,1);
 
         for(i=0; i<plateau.length;i++)
         {
@@ -122,15 +122,15 @@ public class Vue extends JFrame
         }
 
         // panel de l'adversaire et du joueur
-		panoCartesAdversaire = new JPanel();
+        panoCartesAdversaire = new JPanel();
         panoCartesJoueur = new JPanel();
 
         //
-		panoCartesAdversaire.setLayout(new BoxLayout(panoCartesAdversaire, BoxLayout.Y_AXIS));
+        panoCartesAdversaire.setLayout(new BoxLayout(panoCartesAdversaire, BoxLayout.Y_AXIS));
         panoCartesJoueur.setLayout(new BoxLayout(panoCartesJoueur, BoxLayout.Y_AXIS));
 
-		//ajout des scores
-		panoCartesAdversaire.add(scoreJoueur);
+        //ajout des scores
+        panoCartesAdversaire.add(scoreJoueur);
         panoCartesJoueur.add(scoreAdversaire);
 
         // gère l'espacement êntre le score et les cartes
@@ -138,31 +138,32 @@ public class Vue extends JFrame
         panoCartesJoueur.add(Box.createRigidArea(new Dimension(0, 200)));
 
         // placement des cartes dans leurs panel respectifs
-		for(i=0; i<adversaire.length; i++)
-		{
+        for(i=0; i<adversaire.length; i++)
+        {
             panoCartesAdversaire.add(Box.createRigidArea(new Dimension(0, -150)));
             panoCartesAdversaire.add(adversaire[i]);
 
             panoCartesJoueur.add(Box.createRigidArea(new Dimension(0, -150)));
             panoCartesJoueur.add(joueur[i]);
-		}
+        }
         panoJoueur = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
         panoJoueur.add(panoCartesJoueur);
 
         panoAdversaire = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
         panoAdversaire.add(panoCartesAdversaire);
         // panel du chat (miaou)
-		panoChat = new JPanel();
-		panoChat.add(text);
-		panoChat.add(envoyer);
+        panoChat = new JPanel();
+        panoChat.add(text);
+        panoChat.add(envoyer);
 
         // panel principal
-		panoPlace = new JPanel(new BorderLayout());
-		panoPlace.add(panoJoueur, BorderLayout.WEST);
-		panoPlace.add(panoAdversaire, BorderLayout.EAST);
-		panoPlace.add(panoChat, BorderLayout.SOUTH);
-		panoPlace.add(panoPlateau, BorderLayout.CENTER);
+        panoPlace = new JPanel(new BorderLayout());
+        panoPlace.add(panoJoueur, BorderLayout.WEST);
+        panoPlace.add(panoAdversaire, BorderLayout.EAST);
+        panoPlace.add(panoChat, BorderLayout.SOUTH);
+        panoPlace.add(panoPlateau, BorderLayout.CENTER);
 
-		setContentPane(panoPlace);
+        setContentPane(panoPlace);
+
 	}
 }
